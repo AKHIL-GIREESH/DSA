efb6a4ed521449b0a95ff9c2a972c754457533be
Akhil Gireesh <akhilgireeshakg@gmail.com>
2024-05-04 20:50:46 +0530

    cpp

---
+++ C++/00-STL.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a[] = {1, 3, 4};
+    pair<int, int> p;
+    vector<int> v;
+    v.emplace_back(9);
+    p.second = 6;
+    cout << "Hello World" << p.first << v[0];
+    return 0;
+}
