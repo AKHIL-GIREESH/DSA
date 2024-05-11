66000d5dd0821118e93c3c4ef2039f3fd44c9c79
Akhil Gireesh <akhilgireeshakg@gmail.com>
2024-05-11 20:15:18 +0530

    cpp

--- C++/00-STL.cpp
+++ C++/00-STL.cpp
@@ -4,11 +4,26 @@ using namespace std;
 
 int main()
 {
+    // Array
     int a[] = {1, 3, 4};
+
+    // Pair
     pair<int, int> p;
-    vector<int> v;
-    v.emplace_back(9);
     p.second = 6;
-    cout << "Hello World" << p.first << v[0];
+
+    // Vector
+    vector<int> v(5, 10);
+    vector<int> v2(v);
+    v.emplace_back(9);
+    v[0] = 19;
+
+    // iterator
+    for (auto i = v.begin(); i != v.end(); i++)
+    {
+        cout << *(i) << "\n";
+    }
+
+    // Cout
+    cout << "Hello World" << p.first << v[0] << v2[0];
     return 0;
 }
