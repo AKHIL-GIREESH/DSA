0a3fd25a3e4028399d6bdf429f053c4b884b7065
Akhil Gireesh <akhilgireeshakg@gmail.com>
2024-06-08 00:10:15 +0530

    Arrays

--- Arrays/longestSubArraywithSumKMap.cpp
+++ Arrays/longestSubArraywithSumKMap.cpp
@@ -5,7 +5,8 @@ using namespace std;
 
 int main()
 {
-    vector<int> v = {1, 2, 3, 1, 1, 1, 1, 4, 2, 3};
+    vector<int> v1 = {1, 2, 3, 1, 1, 1, 1, 4, 2, 3};
+    vector<int> v = {2, 0, 0, 0, 3};
     map<int, int> m;
     int preSum = 0, l = 0, k = 3, rem;
     for (int i = 0; i < v.size(); i++)
@@ -15,7 +16,7 @@ int main()
         if (preSum == k)
         {
             l = max(l, i + 1);
-            cout << v[i] << endl;
+            // cout << v[i] << endl;
         }
 
         rem = preSum - k;
@@ -24,10 +25,14 @@ int main()
         {
             int len = i - m[rem];
             l = max(l, len);
-            cout << v[i] << endl;
+            // cout << v[i] << endl;
         }
 
-        m[preSum] = i;
+        if (m.find(preSum) == m.end())
+        {
+            // cout << i << endl;
+            m[preSum] = i;
+        }
     }
     cout << l;
     return 0;
