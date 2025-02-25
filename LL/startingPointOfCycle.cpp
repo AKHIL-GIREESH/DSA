a0f1d389d008552c1a02d6bc20cbed0805cc1288
AKHIL-GIREESH <akhilgireeshakg@gmail.com>
2025-02-25 20:50:29 +0530

    Starting Point of a Cycle LL

---
+++ LL/startingPointOfCycle.cpp
@@ -0,0 +1,31 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode *detectCycle(ListNode *head) {
+        ListNode *slow = head, *fast = head;
+        int counter = 0;
+        while(fast != nullptr && fast->next != nullptr){
+            slow = slow->next;
+            fast = fast->next->next;
+
+            if(slow == fast){
+                slow = head;
+                while(slow != fast){
+                    slow = slow->next;
+                    fast = fast->next;
+                    counter++;
+                }
+                return slow;
+            }
+        }
+
+        return nullptr;
+    }
+};
