438c152752471f0418eef47c6de0cf7db4e6b460
AKHIL-GIREESH <akhilgireeshakg@gmail.com>
2025-06-29 12:51:09 +0530

    binary tree

---
+++ Binary Tree/05-width.py
@@ -0,0 +1,44 @@
+from collections import deque
+
+class Node:
+    def __init__(self,data):
+        self.data = data
+        self.left = None
+        self.right = None
+
+def width(node):
+    queue = deque()
+
+    queue.append([node,0])
+    w = 0
+    #print(queue.popleft())
+
+    while(queue):
+        queueLen = len(queue)
+        firstIndex = queue[0][1]
+        lastIndex = queue[-1][1]
+
+        w = max(lastIndex-firstIndex+1,w)
+
+        for _ in range(queueLen):
+            n,i = queue.popleft()
+            l = n.left
+            r = n.right
+
+            if l:
+                queue.append([l,2*i])
+            if r:
+                queue.append([r,2*i+1])
+                
+
+    print(w)
+
+root = Node('A')
+root.left = Node('B')
+root.right = Node('C')
+root.left.left = Node('D')
+root.right.right = Node('E')
+
+width(root)
+
+
